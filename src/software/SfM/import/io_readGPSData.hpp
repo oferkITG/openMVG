1828799089a3a74a08b513eaa0c718c3a9e798e1
Ofer Kruzel <ofer.k@israeltechguard.org>
2023-12-11 07:34:37 +0000

    separate load GPS data to dedicated file

---
+++ src/software/SfM/import/io_readGPSData.hpp
@@ -0,0 +1,119 @@
+#ifndef IO_LOAD_GPS_HPP
+#define IO_LOAD_GPS_HPP
+
+#include "openMVG/sfm/sfm_data.hpp"
+#include "third_party/stlplus3/filesystemSimplified/file_system.hpp"
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <map>
+
+using namespace openMVG;
+using namespace openMVG::sfm;
+
+struct GPS_data{
+    double timestamp_;
+    int gps_id;
+    int frame_id;
+    std::vector<double> parameter_;
+};
+
+//timestamp, frame id, flx, fly, px, py
+struct Frame_Data {
+    double timestamp_;
+    int frame_id;
+};
+
+bool load_GPS_data(const std::string GPS_dir, const SfM_Data sfm_data, std::map<int,GPS_data>& gps_data) {
+
+        std::map<double, Frame_Data> frame_datas;
+
+        std::ifstream frames_data_file(stlplus::create_filespec(GPS_dir, "Frames.txt"), std::ifstream::in);
+        if (!frames_data_file)
+        {
+            std::cerr << "Error: Failed to open file '" << stlplus::create_filespec(GPS_dir, "Frames.txt") << "' for reading" << std::endl;
+            return false;
+        }
+        while (frames_data_file)
+        {
+            std::string line;
+            std::getline(frames_data_file, line);
+            if (line.size() == 0 || line[0] == '#' || !isdigit(line[0]))
+            {
+                continue;
+            }
+
+            Frame_Data temp_frame;
+            std::string substring;
+
+            std::istringstream line_stream(line);
+            std::getline(line_stream, substring, ',');
+            temp_frame.timestamp_ = stod(substring);
+            std::getline(line_stream, substring, ',');
+            temp_frame.frame_id = stoi(substring);
+
+            frame_datas.insert({ temp_frame.timestamp_,temp_frame });
+        }
+        frames_data_file.close();
+
+        std::ifstream gps_data_file(stlplus::create_filespec(GPS_dir, "Anchors.txt"), std::ifstream::in);
+        if (!gps_data_file)
+        {
+            std::cerr << "Error: Failed to open file '" << stlplus::create_filespec(GPS_dir, "Anchors.txt") << "' for reading" << std::endl;
+            return false;
+        }
+        while (gps_data_file)
+        {
+            std::string line;
+            std::getline(gps_data_file, line);
+            if (line.size() == 0 || line[0] == '#' || !isdigit(line[0]))
+            {
+                continue;
+            }
+
+            GPS_data tmp_gps;
+            std::string substring;
+
+            std::istringstream line_stream(line);
+            std::getline(line_stream, substring, ',');
+            tmp_gps.timestamp_ = stod(substring);
+            std::getline(line_stream, substring, ',');
+            tmp_gps.gps_id = stoi(substring);
+            
+            while (std::getline(line_stream, substring, ',')) {
+
+                tmp_gps.parameter_.push_back(stod(substring));
+            }
+
+            std::map<double, Frame_Data>::iterator frm_data_it = frame_datas.find(tmp_gps.timestamp_);
+            if(frm_data_it == frame_datas.end()) {
+              
+              for (frm_data_it = frame_datas.begin(); frm_data_it != frame_datas.end(); frm_data_it++) {
+                std::map<double, Frame_Data>::iterator tmp_frm_data_nxt = std::next(frm_data_it);
+                if((tmp_gps.timestamp_ > frm_data_it->second.timestamp_ && tmp_gps.timestamp_ < tmp_frm_data_nxt->second.timestamp_)) {
+                  break;
+                }
+              }
+
+              assert(frm_data_it != frame_datas.end());
+              
+              std::map<double, Frame_Data>::iterator frm_data_nxt = std::next(frm_data_it);
+              if((tmp_gps.timestamp_ - frm_data_it->second.timestamp_) < (tmp_gps.timestamp_ - frm_data_nxt->second.timestamp_)) 
+                frm_data_it = frm_data_it;
+              else
+                frm_data_it = frm_data_nxt;
+            }
+            
+            Frame_Data frm_data = frm_data_it->second;
+            tmp_gps.frame_id = frm_data.frame_id;
+
+            gps_data.insert({ tmp_gps.frame_id,tmp_gps });
+            std::cout << tmp_gps.frame_id << " " << std::to_string(tmp_gps.timestamp_) << std::endl;
+        }
+        gps_data_file.close();
+
+  return true;
+}
+
+#endif // IO_LOAD_GPS_HPP
