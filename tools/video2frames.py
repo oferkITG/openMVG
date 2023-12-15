49724e2a4a83be175cc8afa7ff88253d75730a02
Ofer Kruzel <ofer.k@israeltechguard.org>
2023-12-15 14:33:00 +0000

    non mp4 support

--- tools/video2frames.py
+++ tools/video2frames.py
@@ -1,6 +1,8 @@
 import os
 import sys
 import cv2
+import moviepy.editor as moviepy
+from tqdm import tqdm
 
 if len(sys.argv) < 1:
     print ("Please provide arkit data folder")
@@ -9,22 +11,28 @@ if len(sys.argv) < 1:
 data_path = sys.argv[1]
 video_file_name = sys.argv[2]
 
-video_path = data_path + video_file_name
+video_path = data_path + "/" + video_file_name
 output_folder = data_path + "/images"
-vidcap = cv2.VideoCapture(video_path)
+
+ext = video_file_name.split('.')[1]
+if ext.lower() != 'mp4':
+    clip = moviepy.VideoFileClip(video_path)
+    clip.write_videofile(video_path.split('.')[0] + '.mp4')
+
+
+vidcap = cv2.VideoCapture(video_path.split('.')[0] + '.mp4')
 
 if not os.path.isdir(output_folder):
     os.makedirs(output_folder) 
 
-success,image = vidcap.read()
-count = 0
-success = True
+length = int(vidcap.get(cv2.CAP_PROP_FRAME_COUNT))
+#success,image = vidcap.read()
 
-while success:
+for count in tqdm(range(length)):
   success,image = vidcap.read()
-  if image is None:
-     break
+  if image is None or not success:
+    print("Failed reading: ", "frame%d.jpg" % count, "\n")
+    break
   image_path = os.path.join(output_folder, "frame%d.jpg" % count)
   cv2.imwrite(image_path, image)     # save frame as JPEG file
-  print("frame: ", count, "Saved image: ", image_path, "success: ", success, "\n")
-  count += 1
+  
